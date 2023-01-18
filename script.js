7303a0f4a79fdbb6d757a10a5eb1c02988d8c081
mnahsanofficial <mnahsanofficial@gmail.com>
2023-01-18 14:10:44 +0600

    grade list updated

--- script.js
+++ script.js
@@ -8,6 +8,27 @@ function calculateGrade() {
   if (marks >= 80 && marks <= 100) {
     document.getElementById("grade").innerText = "A+";
   }
+  else if (marks >= 70 && marks <80) {
+    document.getElementById("grade").innerText = "A";
+  }
+  else if (marks >= 60 && marks <70) {
+    document.getElementById("grade").innerText = "A-";
+  }
+  else if (marks >= 50 && marks <60) {
+    document.getElementById("grade").innerText = "B";
+  }
+  else if (marks >= 40 && marks <50) {
+    document.getElementById("grade").innerText = "C";
+  }
+  else if (marks >= 33 && marks <40) {
+    document.getElementById("grade").innerText = "D";
+  }
+  else if (marks >= 0 && marks <33) {
+    document.getElementById("grade").innerText = "F";
+  }
+  else if (marks <0 || marks >100) {
+    alert("Please check the mark again!!!")
+  }
 }
 
 setInitialValue();
